b8f00e544fdfa05e322d9fe48abdf3e1f5d4a31a
ttran <ttranhub@gmail.com>
2025-07-16 14:37:41 -0700

    Add swagger support

--- src/main/java/com/ttran/demo/rest/GlobalControllerAdvice.java
+++ src/main/java/com/ttran/demo/rest/GlobalControllerAdvice.java
@@ -6,16 +6,26 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
-    @ExceptionHandler(value = { Exception.class })
-    public ResponseEntity<?> handleOtherExceptions(Exception ex, WebRequest request) {
-        String requestUri = ((ServletWebRequest) request).getRequest().getRequestURI();
-        String errorMsg = String.format("Internal Error for request %s: ", requestUri);
-        log.error(errorMsg, ex);
-        return ResponseEntity.internalServerError().body(errorMsg + ex.getMessage());
-    }
+//    @ExceptionHandler(value = { Exception.class })
+//    public ResponseEntity<?> handleOtherExceptions(Exception ex, WebRequest request) {
+//        String requestUri = ((ServletWebRequest) request).getRequest().getRequestURI();
+//        String errorMsg = String.format("Internal Error for request %s: ", requestUri);
+//        log.error(errorMsg, ex);
+//        return ResponseEntity.internalServerError().body(errorMsg + ex.getMessage());
+//    }
+//
+//    @ExceptionHandler(value = { NoResourceFoundException.class })
+//    public ResponseEntity<?> ClientExceptions(Exception ex, WebRequest request) {
+//        String requestUri = ((ServletWebRequest) request).getRequest().getRequestURI();
+//        String errorMsg = String.format("Client Error for request %s: ", requestUri);
+//        log.error(errorMsg, ex);
+//        return ResponseEntity.badRequest().body(errorMsg + ex.getMessage());
+//    }
+
 }
